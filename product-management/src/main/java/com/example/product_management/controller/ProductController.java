3108ab38811284509e13c95bd863d758f33c5083
Lê Cảnh Toàn <lcnhton@gmail.com>
2025-11-29 11:35:16 +0700

    Quick fix

--- product-management/src/main/java/com/example/product_management/controller/ProductController.java
+++ product-management/src/main/java/com/example/product_management/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.example.product_management.controller;
 
 import com.example.product_management.entity.Product;
 import com.example.product_management.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
